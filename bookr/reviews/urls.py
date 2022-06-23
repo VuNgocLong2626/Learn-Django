ebef722b2d73dea74d3e738136cf79ed0ab4d80f
Vu_Long <long.speed00@gmail.com>
2022-06-24 00:24:56 +0700

    chapter 12 fis

--- bookr/reviews/urls.py
+++ bookr/reviews/urls.py
@@ -1,7 +1,19 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+from . import views, api_views
+
+from rest_framework.routers import DefaultRouter
+
+from . import views, api_views
+
+router = DefaultRouter()
+router.register(r'books', api_views.BookViewSet)
+router.register(r'reviews', api_views.ReviewViewSet)
 
 urlpatterns = [
+    # path('api/contributors/', api_views.ContributorView.as_view(), name='contributors'),
+    # path('api/all_books/', api_views.AllBooks.as_view(), name='all_books'),
+    path('api/login', api_views.Login.as_view(), name='login'),
+    path('api/', include((router.urls, 'api'))),
     path('', views.index),
     path('books/', views.book_list, name='book_list'),
     path('books/<int:pk>/', views.book_detail, name='book_detail'),
